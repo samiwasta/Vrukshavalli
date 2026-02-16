34bd66efe16c79f8b5cd35cd7bcf90b23e1baffe
samiwasta <samiwasta.11@gmail.com>
2026-02-17 02:24:55 +0530

    fix: correct import paths for case-sensitive file systems

--- app/features/navbar/Navbar.tsx
+++ app/features/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import NavbarDesktop from "@/app/features/Navbar/NavbarDesktop";
-import NavbarMobile from "@/app/features/Navbar/NavbarMobile";
+import NavbarDesktop from "@/app/features/navbar/NavbarDesktop";
+import NavbarMobile from "@/app/features/navbar/NavbarMobile";
 
 export default function Navbar() {
   return (
